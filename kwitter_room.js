1572dc88a9fef7656e9d23eef78869a0040cc834
si-guy1 <82059095+si-guy1@users.noreply.github.com>
2022-01-18 11:35:55 -0500

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBV7sD9AhvsvxVFzzD8vXIkVb7_W-kpvoo",
+    authDomain: "let-s-chat-chat-app.firebaseapp.com",
+    databaseURL: "https://let-s-chat-chat-app-default-rtdb.firebaseio.com",
+    projectId: "let-s-chat-chat-app",
+    storageBucket: "let-s-chat-chat-app.appspot.com",
+    messagingSenderId: "333391934991",
+    appId: "1:333391934991:web:462ee4e33aea4905e7d57a",
+  };
+  var app = initializeApp(firebaseConfig);
